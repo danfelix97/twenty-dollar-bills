d134249082a58accce742131b4887fdab0f51cf5
Daniel Felix <117397928+danfelix97@users.noreply.github.com>
2026-02-27 11:14:29 -0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,21 +3,15 @@ using namespace std;
 
 int main() {
 
-	//declarations
 	double userAmount;
 	int numOfBills;
 	double billAmount = 20.0;
 
-	//get user input
 	cout << "Enter the amount given to the teller: $";
 	cin >> userAmount;
 
-	//calculation
-	//numOfBills = userAmount / 20; // <-- results in a implicit conversion of a double
 	numOfBills = static_cast<int>(userAmount / billAmount);
-	//numOfBills = (int)(userAmount / 20.0); //c-like cast notation
 
-	//output
 	cout << "If you give me $" << userAmount 
 		<< ", I'll give you " << numOfBills << " $20 bills" << endl;
 
